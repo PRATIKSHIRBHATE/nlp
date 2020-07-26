8647e367fde88ce21bd881f830faf21c1a92395a
PRATIKSHIRBHATE <pratikshirbhate@gmail.com>
2020-07-26 18:14:15 +0530

    added a jupyter notebook for word cloud creation

---
+++ nlp_tutorial.py
@@ -0,0 +1,16 @@
+# NLP Tutorial
+
+# import nltk
+
+# nltk.download()
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_sentence = "Hello Mr. Pratik, how are you doing today? You are genius and I admire your hardwork too."
+
+# Split example sentence by words and sentence
+print(sent_tokenize(example_sentence))
+
+# print(word_tokenize(example_sentence))
+
+# sentence = "Hi my name is Pratik. I am a Data Scientist and I am excited to learn NLP through this tutorial"
